26cc63c251374342df5e0527d27d96c0f9484e85
Ariel Saldana <ariel@unreal.io>
2024-03-02 10:01:36 -0500

    adding home.nix

--- etc/nixos/configuration.nix
+++ etc/nixos/configuration.nix
@@ -8,6 +8,7 @@
   imports =
     [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
+      <home-manager/nixos>
     ];
 
   # Bootloader.
@@ -109,6 +110,7 @@
   environment.systemPackages = with pkgs; [
   #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
   #  wget
+    home-manager
   ];
 
   # Some programs need SUID wrappers, can be configured further or are
